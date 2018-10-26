84df2d091635886f17fe63f13c4034c42996eef7
Justin Singh <justin.singh125125@gmail.com>
2018-10-26 16:21:37 -0700

    added support for movie

--- liri.js
+++ liri.js
@@ -1,13 +1,16 @@
 //this includes our dotenv file into the program
 require("dotenv").config();
+var request = require("request");
 
 //creates a new sportify variable that holds files from the spotify api
 var Spotify = require('node-spotify-api');
+
 //the keys variable holds api keys
 var keys = require('./keys.js');
 
 //create an instance of the spotify variable along with the api keys
 var spotify = new Spotify(keys.spotify);
+
 
 //this will select which function or app we are going to execute
 var selectApp = process.argv[2];
@@ -19,10 +22,46 @@ var contentApp = initData();
 
 //create constant variables for the list of selections
 const selectionSpofity = "spotify-this-song";
+const selectionBands = "concert-this";
+const selectionMovie = "movie-this";
 
 //selects the options 
 selectOptions();
 
+
+function getMovie() {
+    var queryUrl = "http://www.omdbapi.com/?t=" + contentApp + "&y=&plot=short&apikey=trilogy";
+
+    request(queryUrl, function (error, response, body) {
+
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            var temp = JSON.parse(body);
+            console.log("---------------------");
+
+            console.log("Movie Name: " + temp.Title);
+            console.log("Release Date: " + temp.Released);
+            console.log("Rotten Tomatoes Rating: " + temp.Ratings[1].Value);
+            console.log("Country produced: " + temp.Country);
+            console.log("Language: " + temp.Language);
+            console.log("Plot: " + temp.Plot);
+            console.log("Actors: " + temp.Actors);
+        }
+    });
+
+}
+
+function getBands() {
+
+    request("http://rest.bandsintown.com/artists/{bruno}/events?app_id=400d5aa1c8515365e2be7cf76cfd8ad9", function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(body);
+        }
+    });
+}
 
 
 
@@ -47,6 +86,12 @@ function getSpotify() {
 function selectOptions() {
     if (selectApp == selectionSpofity) {
         getSpotify();
+    }
+    if (selectApp == selectionBands) {
+        getBands();
+    }
+    if (selectApp == selectionMovie) {
+        getMovie();
     }
 }
 
