ec29371fc935c15d61b86e1d4492ed066d1ed74a
Justin Singh <chaosdoggs@Justins-MBP.frontierlocal.net>
2018-10-26 09:13:22 -0700

    it works

--- liri.js
+++ liri.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 
-var keys = require('./keys');
+var Spotify = require('node-spotify-api');
+var keys = require('./keys.js');
 
 
 var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+console.log(spotify);
 
